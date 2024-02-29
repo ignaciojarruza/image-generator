843a2eea1d3ec280d8a79cb12de17a9a0cbe96dc
ignaciojarruza <85847657+ignaciojarruza@users.noreply.github.com>
2024-02-29 17:09:24 -0500

    sample GET

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 import OpenAI from "openai";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 const openai = new OpenAI();
 
-async function main() {
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", async (req, res) => {
   const completion = await openai.chat.completions.create({
-    messages: [
-      { role: "system", content: "Repeat the last message I sent to you." },
-    ],
+    messages: [{ role: "system", content: "Hello World" }],
     model: "gpt-3.5-turbo",
   });
-  console.log(completion.choices[0]);
-}
+  res.json({ completion: completion.choices[0] });
+});
 
-main();
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
